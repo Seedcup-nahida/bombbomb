bd3f2ccaf47c9b3d208247248bf922690509250a
lixian-lipton <2480050860@qq.com>
2023-11-23 21:09:51 +0800

    complete:value.py(except init_value) modify:bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ enum ActionType:// 动作类型
 
 
 box_count = 0
+judge_level2 = 0.1
 
 
 def game_update(player_id: int, round: int, map: list[list[dict]], players: list[dict], bombs: list[dict], items: list[list[dict]]) -> list[ActionType]:
@@ -23,6 +24,15 @@ def game_update(player_id: int, round: int, map: list[list[dict]], players: list
     return a list of values of ActionType in req.py and length less than {player.speed}
     """
     # TODO
+    box = 0
+    level = 1
+    for block_row in map:
+        for b in block_row:
+            if BlockType.BOX.match(b):
+                box += 1
+    if box <= box_count * judge_level2:
+        level = 2
+
     step = 2
     actions = []  # ATTENTION: len(actions) <= player.speed, equal to player.speed is recommended
     map_danger = scan_map_danger(map, bombs, players, player_id)
@@ -32,7 +42,7 @@ def game_update(player_id: int, round: int, map: list[list[dict]], players: list
         actions.append(action)
     step -= len(actions_danger)
     if step > 0:
-        actions_value = scan_value(step, player_id, round, map, players, bombs, items, map_danger)
+        actions_value = scan_value(step, player_id, map, players, items, map_danger, level)
         for action in actions_value:
             actions.append(action)
     return actions
--- value.py
+++ value.py
@@ -1,16 +1,35 @@
+from random import *
 from bot_base import *
-from value import *
 
 direction_list = [[0, -1], [0, 1], [1, 0], [-1, 0]]
+judge_value_gap = 150
+distance_decay = 0.7
 
-def scan_value(step, player_id, round, map, players, bombs, items, map_danger):
+def scan_value(step, player_id, map, players, items, map_danger, level):
     map_value = [[0 for _ in range(15)] for _ in range(15)]
+    actions = []
+    player = get_current_player(players, player_id)
+    enemies = get_enemy_players(players, player_id)
+
     for x in range(15):
         for y in range(15):
-            mark_value(x, y, map, items, map_value)
+            mark_value(x, y, map, items, map_value, level, player, enemies)
+
+    if player["bomb_now_num"] > 0:
+        actions.append(put_bomb(player, map, items, enemies, map_value, level))
+    if len(actions):
+        step -= 1
+    if step == 0:
+        return actions
+
+    move_action = move(step, player, map, map_value, map_danger, enemies, level)
+    for action in move_action:
+        actions.append(action)
+
+    return actions
 
 
-def mark_value(x, y, map, items, map_value):
+def mark_value(x, y, map, items, map_value, level, player, enemies):
     if not items[x][y]:
         for i in range(x - 1, x + 2):
             for j in range(y - 1, y + 2):
@@ -20,7 +39,7 @@ def mark_value(x, y, map, items, map_value):
                     map_value += 10
         return
 
-    queue=[{"x": x, "y": y, "depth": 0, "value": init_value(items[x][y])}]
+    queue=[{"x": x, "y": y, "depth": 0, "value": init_value(items[x][y], level, player, enemies)}]
     depth_max = 6
 
     while queue:
@@ -29,17 +48,99 @@ def mark_value(x, y, map, items, map_value):
             status["x"] += direction[0]
             status["y"] += direction[1]
             status["depth"] += 1
-            status["value"] *= 0.75
+            status["value"] *= distance_decay
             if status["x"] < 0 or status["x"] >= len(map) or status["y"] < 0 or status["y"] >= len(map[0]):
                 continue
             elif status["depth"] > depth_max:
                 continue
             elif not BlockType.BLOCK.match(map[status["x"]][status["y"]]):
                 queue.append(status)
-                map_value[status["x"]][status["y"]] += status["value"]
+                map_value[status["x"]][status["y"]] = max(map_value[status["x"]][status["y"]], status["value"])
         del queue[0]
 
 
-def init_value(ItemType):
+def put_bomb(player, map, items, enemies, map_value, level):
+    box = []
+
+    for direction in direction_list:
+        x = player["x"]
+        y = player["y"]
+        for i in player["bomb_range"]:
+            x += direction[0]
+            y += direction[1]
+            if x >= 0 and x < len(map) and y >= 0 and y < len(map[0]):
+                if BlockType.BLOCK.match(map[x][y]):
+                    if map[x][y] & 2:
+                        box.append([x, y])
+                    break
+                elif items[x][y] > 0:
+                    return
+                else:
+                    for enemy in enemies:
+                        if x == enemy["x"] and y == enemy["y"]:
+                            return 5
+    if level == 1 and len(box) >= 2:
+        return 5
+    elif len(box) == 0:
+        return
+
+    x = player["x"]
+    y = player["y"]
+    before_put_value = map_value[x][y]
+    after_map = map
+    after_map_value = [[0 for _ in range(15)] for _ in range(15)]
+    for single_box in box:
+        after_map[single_box[0]][single_box[1]] = 0
+    for i in range(15):
+        for j in range(15):
+            map_value(i, j, after_map, items, after_map_value)
+    after_put_value = after_map_value[x][y]
+    for direction in direction_list:
+        x += direction[0]
+        y += direction[1]
+        if x >= 0 and x < len(map) and y >= 0 and y < len(map[0]):
+            before_put_value += map_value[x][y]
+            after_put_value += after_map_value[x][y]
+    if (after_put_value - before_put_value) >= judge_value_gap:
+        return 5
+
+
+def move(step, player, map, map_value, map_danger, enemies, level):
+    actions = []
+    before_x = player["x"]
+    before_y = player["y"]
+
+    for i in range(step):
+        value_max = -1
+        path = []
+        for direction in direction_list:
+            x = before_x + direction[0]
+            y = before_y + direction[1]
+            if x >= 0 and x < len(map) and y >= 0 and y < len(map[0]):
+                if map_danger[x][y] > 0:
+                    continue
+                elif map_value[x][y] > value_max:
+                    value_max = map_value[x][y]
+                    path = direction
+                elif map_value[x][y] == value_max:
+                    distance_change = 0
+                    for enemy in enemies:
+                        distance_change += abs(x - enemy["x"]) + abs(y - enemy["y"])
+                        distance_change -= abs(before_x + path[0] - enemy["x"]) + abs(before_y + path[1] - enemy["y"])
+                    if level == 1 and distance_change > 0 or level == 2 and distance_change < 0:
+                        path = direction
+                    elif distance_change == 0:
+                        if random() % 2:
+                            path = direction
+        if not len(path):
+            break
+        actions.append(convert_list_to_direction(path))
+        before_x += path[0]
+        before_y += path[1]
+
+    return actions
+
+
+def init_value(item_type, level, player, enemies):
     pass
     return 1
